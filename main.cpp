ffb7f24c210683b55095373d2999f0a3006811df
flyingbest <taeyoon4715@gmail.com>
2017-02-16 16:12:39 +0900

    sturcture

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <cstring>		// Not using STL.
+#include <cstring>		// Not using STL version.
 using namespace std;
 
 #define ACC_LEN 25
 #define NAME_LEN 20
+#define FIN_LEN 20
 
 enum { OPEN=1, DEPOSIT, WITHDRAW, DISPLAY, EXIT };
 
@@ -11,37 +12,46 @@ class Account{
 private:
 	char *acc_id;
 	char *acc_holder;
+	char *fin_ins_name;
 	int balance;
 
 public:
-	Account(int money, char *id, char *name);
+	Account(int money, char *id, char *name, char *fin_name);
 	Account(const Account & ref);
 
 	char* GetAccId() const;
 	char* GetAccHolder() const;
+	char* GetFin_name() const;
 	void Deposit(int money);
 	int Withdraw(int money);
 	void ShowCurInfo() const;
 	~Account();
 };
 
-Account::Account(int money, char *id, char *name) : balance(money){
+Account::Account(int money, char *id, char *name, char *fin_name) : balance(money){
 	acc_id = new char[strlen(id)+1];
 	strcpy(acc_id, id);
 	acc_holder = new char[strlen(name)+1];
 	strcpy(acc_holder, name);
+	fin_ins_name = new char[strlen(fin_name)+1];
+	strcpy(fin_ins_name, fin_name);
 }
 Account::Account(const Account & ref) : balance(ref.balance){
 	acc_id = new char[strlen(ref.acc_id)+1];
 	strcpy(acc_id, ref.acc_id);
 	acc_holder = new char[strlen(ref.acc_holder)+1];
 	strcpy(acc_holder, ref.acc_holder);
+	fin_ins_name = new char[strlen(ref.fin_ins_name)+1];
+	strcpy(fin_ins_name, ref.fin_ins_name);
 }
 char* Account::GetAccId() const{
 	return acc_id;
 }
 char* Account::GetAccHolder() const{
 	return acc_holder;
+}
+char* Account::GetFin_name() const{
+	return fin_ins_name;
 }
 void Account::Deposit(int money){
 	balance += money;
@@ -54,13 +64,16 @@ int Account::Withdraw(int money){
 	return money;
 }
 void Account::ShowCurInfo() const{
-	cout << "Account ID : " << acc_id << endl;
-	cout << "Account HOLDER : " << acc_holder << endl;
-	cout << "Cur Balance : " << balance << endl;
+	cout << "ID       : " << acc_id << endl;
+	cout << "HOLDER   : " << acc_holder << endl;
+	cout << "FIN_NAME : " << fin_ins_name << endl;
+	cout << "BALANCE  : " << balance << endl;
+	cout << "====================================" << endl;
 }
 Account::~Account(){
 	delete []acc_id;
 	delete []acc_holder;
+	delete []fin_ins_name;
 }
 
 class AccountControler{
@@ -83,86 +96,124 @@ AccountControler::AccountControler() : ArrNum(0){
 }
 
 void AccountControler::DisplayMenu() const{
-	cout << "----------Menu----------" << endl;
-	cout << "1. Open an new Account. " << endl;
-	cout << "2. Deposit money. " << endl;
-	cout << "3. Withdraw money. " << endl;
-	cout << "4. Display all account info. " << endl;
-	cout << "5. Exit the program. " << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|               MENU               |" << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|  1.  Open an new Account.        |" << endl;
+	cout << "|  2.  Deposit money.              |" << endl;
+	cout << "|  3.  Withdraw money.             |" << endl;
+	cout << "|  4.  Display all account info.   |" << endl;
+	cout << "|  5.  Exit the program.           |" << endl;
+	cout << "+----------------------------------+" << endl;
 }
 
 void AccountControler::OpenAccount(){
 	char id[ACC_LEN];
 	char name[NAME_LEN];
+	char fin_name[FIN_LEN];
 	int money;
 
-	cout << "|| Open an Account ||" << endl;
-	cout << "Account ID : ";
+	cout << "+----------------------------------+" << endl;
+	cout << "|         Opne an Account          |" << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "Account ID     : ";
 	cin >> id;
-	cout << "Account HOLDER : ";
+	cout << "Account HOLDER	: ";
 	cin >> name;
-	cout << "Deposit Amount : ";
+	cout << "Fin_ins NAME   : ";
+	cin >> fin_name;
+	cout << "First Deposit Amount : ";
 	cin >> money; 
 	cout << endl;
 
 	for(int i=0; i<ArrNum; ++i){
 		if(!strcmp(AccArr[i]->GetAccId(), id)){
-			cout << "Account ID is already exist!" << endl << endl;
+			cout << "+----------------------------------+" << endl;
+			cout << "|  [Account ID is already exist!]  |" << endl;
+			cout << "+----------------------------------+" << endl << endl;
 			return;
 		}
 	}
 	
-	AccArr[ArrNum++] = new Account(money, id, name);
+	AccArr[ArrNum++] = new Account(money, id, name, fin_name);
 }
 
 void AccountControler::DepositMoney(){
 	int money;
 	char id[ACC_LEN];
 	
-	cout << "|| DEPOSIT ||" << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|             DEPOSIT              |" << endl;
+	cout << "+----------------------------------+" << endl;
 	cout << "Account id : ";
 	cin >> id;
-	cout << "Deposit Amount : ";
+	cout << "Amount     : ";
 	cin >> money;
 
 	for(int i=0; i<ArrNum; ++i){
 		if(!strcmp(AccArr[i]->GetAccId(), id)){
-			AccArr[i]->Deposit(money);
-			cout << "Deposit Complete." << endl << endl;
-			return;
+			if(money <= 0){
+				cout << "+----------------------------------+" << endl;
+				cout << "|    Can not deposit below $0.     |" << endl;
+				cout << "+----------------------------------+" << endl << endl;
+				return;
+			}else{	
+				AccArr[i]->Deposit(money);
+				cout << "+----------------------------------+" << endl;
+				cout << "|        [Deposit Complete.]       |" << endl;
+				cout << "+----------------------------------+" << endl << endl;
+				return;
+			}
 		}
 	}
-	cout << "Account Id is not Valid!" << endl << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|    [Account Id is not Valid!]    |" << endl;
+	cout << "+----------------------------------+" << endl << endl;
 }
 
 void AccountControler::WithdrawMoney(){
 	int money;
 	char id[ACC_LEN];
 
-	cout << "|| WITHDRAW ||" << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|            WITHDRAW              |" << endl;
+	cout << "+----------------------------------+" << endl;
 	cout << "Account id : ";
 	cin >> id;
-	cout << "Withdraw Amount : ";
+	cout << "Amount     : ";
 	cin >> money;
 	
 	for(int i=0; i<ArrNum; ++i){
 		if(!strcmp(AccArr[i]->GetAccId(), id)){
 			if(AccArr[i]->Withdraw(money) == 0){
-				cout << "Not enough money!" << endl << endl;
+				cout << "+----------------------------------+" << endl;
+				cout << "|       [Not enough money!]        |" << endl;
+				cout << "+----------------------------------+" << endl << endl;
 				return;
 			}
-			cout << "Withdraw Complete." << endl << endl;
+			cout << "+----------------------------------+" << endl;
+			cout << "|       [Withdraw Complete.]       |" << endl;
+			cout << "+----------------------------------+" << endl << endl;
 			return;
 		}
 	}
-	cout << "Account Id is not Valid!" << endl << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|    [Account Id is not Valid!]    |" << endl;
+	cout << "+----------------------------------+" << endl << endl;
 }
 
 void AccountControler::DisplayAccInfo() const{
+	cout << "+----------------------------------+" << endl;
+	cout << "|           ACCOUNT LIST           |" << endl;
+	cout << "+----------------------------------+" << endl << endl;
+	cout << "====================================" << endl;
 	for(int i=0; i<ArrNum; ++i){
+		cout << "No. " << i+1 << " Account Info." << endl;
 		AccArr[i]->ShowCurInfo();
-		cout << endl;
 	}
+	cout << "+----------------------------------+" << endl;
+	cout << "|           END OF LIST            |" << endl;
+	cout << "+----------------------------------+" << endl << endl;
 }
 
 AccountControler::~AccountControler(){
@@ -174,10 +225,17 @@ AccountControler::~AccountControler(){
 int main(int argc, char * argv[]){
 	AccountControler manager;
 	int selectNum;
+
+	cout << "+----------------------------------+" << endl;
+	cout << "|        MY ACCOUNT SYSTEM         |" << endl;
+	cout << "+----------------------------------+" << endl;
+	cout << "|            Made by Taeyoon Moon. |" << endl;
+	cout << "|                  (Version 1.0.0) |" << endl;
+	cout << "+----------------------------------+" << endl << endl;
 	
-	while(1){
+	while(true){
 		manager.DisplayMenu();
-		cout << "select number : ";
+		cout << "Select Number(only number) : ";
 		cin >> selectNum;
 		cout << endl;
 
@@ -195,11 +253,14 @@ int main(int argc, char * argv[]){
 				manager.DisplayAccInfo();
 				break;
 			case EXIT:
-				cout << "Thank you for using. Have a nice day!" << endl;
+				cout << "+---------------------------------------+" << endl;
+				cout << "| Thank you for using! Have a nice day! |" << endl;
+				cout << "+---------------------------------------+" << endl;
 				return 0;
 			default:
-				cout << "Illegal selection! enter again.." << endl;
-				break;
+				cout << "+----------------------------------+" << endl;
+				cout << "|   Wrong Number! select again!    |" << endl;
+				cout << "+----------------------------------+" << endl;
 			}
 	}
 	return 0;
